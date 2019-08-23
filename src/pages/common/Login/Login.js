fea197b69c9b52f3d78d6d46362be2313ab312c3
wq1031855643 <1031855643@qq.com>
2019-08-23 18:58:28 +0800

    update

--- src/pages/common/Login/Login.js
+++ src/pages/common/Login/Login.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react'
 import './style.scss'
+import HeadBack from '../../../components/head-back/headBack'
 
 export default class Login extends Component {
     render() {
         return (
-            <div className="login-box">
-                <div className="form-box">
-                    <div className="input-box">
-                        <span>账户：</span>
-                        <input type="text" placeholder="请输入手机号" />
-                    </div>
-                    <div className="input-box">
-                        <span>密码：</span>
-                        <input type="password" placeholder="请输入密码" />
-                    </div>
-                    <div className="btn-box">
-                        <button>登录</button>
-                    </div>
-                    <div className="reg-box">
-                        <p>忘记密码</p>
-                        <p onClick={()=>this.toRegister()}>注册</p>
+            <>
+                <HeadBack title="登录" info={this.props} />
+                <div className="login-box">
+                    <div className="form-box">
+                        <div className="input-box">
+                            <span>账户：</span>
+                            <input type="text" placeholder="请输入手机号" />
+                        </div>
+                        <div className="input-box">
+                            <span>密码：</span>
+                            <input type="password" placeholder="请输入密码" />
+                        </div>
+                        <div className="btn-box">
+                            <button>登录</button>
+                        </div>
+                        <div className="reg-box">
+                            <p>忘记密码</p>
+                            <p onClick={()=>this.toRegister()}>注册</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </>
         )
     }
     toRegister(){
