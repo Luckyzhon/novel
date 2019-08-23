48e5438f14309e867f58b49ae0c6d22da61da6b1
wq1031855643 <1031855643@qq.com>
2019-08-23 22:02:13 +0800

    update

--- src/pages/common/Login/Login.js
+++ src/pages/common/Login/Login.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import './style.scss'
+import {connect} from 'react-redux'
 import HeadBack from '../../../components/head-back/headBack'
+import {requestLoginAction} from '../../../store/modules/user'
 
-export default class Login extends Component {
+import './style.scss'
+
+
+class Login extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
     render() {
+        let {loginAction} = this.props;
+        let {username, password} = this.state;
         return (
             <>
                 <HeadBack title="登录" info={this.props} />
@@ -11,14 +21,14 @@ export default class Login extends Component {
                     <div className="form-box">
                         <div className="input-box">
                             <span>账户：</span>
-                            <input type="text" placeholder="请输入手机号" />
+                            <input value={username} onChange={this.userNameChangeAction} type="text" placeholder="请输入手机号" />
                         </div>
                         <div className="input-box">
                             <span>密码：</span>
-                            <input type="password" placeholder="请输入密码" />
+                            <input value={password} onChange={this.passWordChangeAction} type="password" placeholder="请输入密码" />
                         </div>
                         <div className="btn-box">
-                            <button>登录</button>
+                            <button onClick={()=>loginAction(username, password, this.props.history)}>登录</button>
                         </div>
                         <div className="reg-box">
                             <p>忘记密码</p>
@@ -32,4 +42,37 @@ export default class Login extends Component {
     toRegister(){
         this.props.history.push('/register');
     }
+
+    userNameChangeAction = (eve)=> {
+        this.setState({
+            username: eve.target.value
+        })
+    }
+
+    passWordChangeAction = (eve)=> {
+        this.setState({
+            password: eve.target.value
+        })
+    }
 }
+
+const mapStateToProps = (state)=>({
+
+})
+
+
+const mapDispatchToProps = (dispatch)=>({
+    //登录
+    loginAction(username, password, history){
+        if(!(/(^1[3|4|5|7|8]\d{9}$)|(^09\d{8}$)/.test(username))){
+            alert('手机号格式不正确!');
+            return;
+        }
+
+        // 发送请求给后台执行登录
+        let action = requestLoginAction(username, password, history);
+        dispatch(action);
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
